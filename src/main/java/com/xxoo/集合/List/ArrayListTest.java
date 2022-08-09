2c15d607a6f2d4f8706f146c130e786df3f7c77c
闫绍华 <923596260@qq.com>
2022-08-09 18:56:16 +0800

    集合

---
+++ src/main/java/com/xxoo/集合/List/ArrayListTest.java
@@ -0,0 +1,25 @@
+package com.xxoo.集合.List;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author yanshaohua
+ * @Date 2022/7/19 11:18
+ * @Description: TODO
+ */
+public class ArrayListTest {
+
+    public static void main(String[] args) {
+
+        List<String> result = new ArrayList<>();
+
+
+
+
+    }
+
+
+
+
+}
---
+++ src/main/java/com/xxoo/集合/Map/HashMapTest.java
@@ -0,0 +1,19 @@
+package com.xxoo.集合.Map;
+
+import java.util.HashMap;
+
+/**
+ * @Author yanshaohua
+ * @Date 2022/7/19 12:06
+ * @Description: TODO
+ */
+public class HashMapTest {
+
+    public static void main(String[] args) {
+
+        HashMap<Object, Object> map = new HashMap<>();
+        map.put(1,"张三");
+
+
+    }
+}
